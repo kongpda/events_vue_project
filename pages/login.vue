c5d8a88cacdf9e5fb648028efd73de06053f7f66
kongpda <kongpda@gmail.com>
2024-10-15 00:18:52 +0700

    Update login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -1,13 +1,9 @@
 <template>
     <div class="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-md">
-            <img
-                class="mx-auto h-10 w-auto"
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company"
-            />
+            <CompanyLogo />
             <h2 class="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                Sign in to your account
+                {{ $t('signIn') }}
             </h2>
         </div>
 
@@ -133,8 +129,14 @@
             <p class="mt-6 text-center text-sm text-gray-500">
                 Not a member?
                 {{ ' ' }}
-                <a href="/signup" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Sign up</a>
+                <a href="/signup" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">{{
+                    $t('signUp')
+                }}</a>
             </p>
         </div>
     </div>
 </template>
+
+<script setup>
+import CompanyLogo from '@/components/logo/CompayLogo.vue'
+</script>
