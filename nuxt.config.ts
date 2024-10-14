fa3240f536470e5054f997948e0253e4b0c9006f
kongpda <kongpda@gmail.com>
2024-10-14 20:02:27 +0700

    update
    install and setup nuxtjs/i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,21 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
-},
+  },
+
+  modules: ['@nuxtjs/i18n'],
+  i18n: {
+    locales: [
+      { code: 'en', language: 'en-US', file: 'locale/en-Us.json' },
+      { code: 'kh', language: 'kh-KH', file: 'locale/kh-Kh.json' }
+    ],
+    defaultLocale: 'en',
+    strategy: 'prefix_and_default',
+  },
 })
